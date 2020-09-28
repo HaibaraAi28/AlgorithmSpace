6d809eaf618e21b342a6766b948c3f24bc5a21a7
yckj2791 <fanchengde@cloudwalk.com>
2020-09-29 04:21:40 +0800

    栈和队列

---
+++ src/main/java/com/fcd/algorithmlclass/StackAndQueue/TwoStacksImplementQueue.java
@@ -0,0 +1,66 @@
+package com.fcd.algorithmlclass.StackAndQueue;
+
+import java.util.Stack;
+
+/**
+ * 两个栈实现队列
+ * 弹出时，将push栈中元素倒入pop栈
+ * 倒入前提：1.pop栈为空
+ *         2.push栈要保证倒空
+ * 否则，直接弹出pop栈栈顶
+ */
+public class TwoStacksImplementQueue {
+    public static class TwoStacksQueue {
+        public Stack<Integer> stackPush;
+        public Stack<Integer> stackPop;
+
+        public TwoStacksQueue() {
+            stackPush = new Stack<Integer>();
+            stackPop = new Stack<Integer>();
+        }
+
+        // push栈向pop栈倒入数据
+        private void pushToPop() {
+            if (stackPop.empty()) {
+                while (!stackPush.empty()) {
+                    stackPop.push(stackPush.pop());
+                }
+            }
+        }
+
+        public void add(int pushInt) {
+            stackPush.push(pushInt);
+            pushToPop();
+        }
+
+        public int poll() {
+            if (stackPop.empty() && stackPush.empty()) {
+                throw new RuntimeException("Queue is empty!");
+            }
+            pushToPop();
+            return stackPop.pop();
+        }
+
+        public int peek() {
+            if (stackPop.empty() && stackPush.empty()) {
+                throw new RuntimeException("Queue is empty!");
+            }
+            pushToPop();
+            return stackPop.peek();
+        }
+    }
+
+    public static void main(String[] args) {
+        TwoStacksQueue test = new TwoStacksQueue();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+    }
+
+}
